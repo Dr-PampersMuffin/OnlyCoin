80a2c4b250dc31af835bcf592b8da993a6d234c2
OnlyCoinToken <pampersmuffin@gmail.com>
2025-06-10 17:43:21 +0300

    Update OnlyCoin.sol

--- OnlyCoin.sol
+++ OnlyCoin.sol
@@ -1,11 +1,48 @@
+/**
+ *Submitted for verification at BscScan.com on 2025-05-30
+*/
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract OnlyCoin {
+    string public name = "Only Coin";
+    string public symbol = "OnlyCoin";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 21000000 * 10**uint256(decimals);
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    address public owner;
 
-contract OnlyCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("OnlyCoin", "OnlyCoin") {
-        _mint(msg.sender, initialSupply);
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance.");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value, "Insufficient balance.");
+        require(allowance[from][msg.sender] >= value, "Allowance exceeded.");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
     }
 }
